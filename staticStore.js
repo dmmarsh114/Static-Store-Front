33cf70d4562441e1ae5917a80f86ffc48f2700fa
dmmarsh114 <49419067+dmmarsh114@users.noreply.github.com>
2020-08-22 15:53:14 -0400

    first day of work

---
+++ staticStore.js
@@ -0,0 +1,41 @@
+let cardContainer = document.getElementById('card-list');
+let cards = [];
+
+// add test cards! ============================================
+for (let i = 1; i <= 6; i++) {
+    let newItem = {
+        name: `Item ${i}`,
+        text: "Bilged on her anchor spanker hail-shot keelhaul schooner bounty rope's end Davy Jones' Locker no prey, no pay doubloon. Doubloon Privateer quarter avast boom skysail sloop Barbary Coast knave mizzen.",
+    }
+    cards.push(newItem);
+}
+
+cards.forEach(i => {
+    let cardHolder = document.createElement('li');
+
+    let card = document.createElement('div');
+    card.classList.add('card');
+
+    let cardTitle = document.createElement('h3');
+    cardTitle.innerText = i.name;
+    cardTitle.classList.add('card-title');
+    card.appendChild(cardTitle);
+
+    let cardDesc = document.createElement('p');
+    cardDesc.innerText = i.text;
+    cardDesc.classList.add('card-text');
+    card.appendChild(cardDesc);
+
+    let cardBtnContainer = document.createElement('div');
+    cardBtnContainer.classList.add('card-btn-container');
+    card.appendChild(cardBtnContainer);
+
+    let cardBtn = document.createElement('i');
+    cardBtn.innerText = 'add_shopping_cart';
+    cardBtn.classList.add('material-icons');
+    cardBtn.classList.add('card-btn');
+    cardBtnContainer.appendChild(cardBtn);
+
+    cardHolder.appendChild(card);
+    cardContainer.appendChild(cardHolder);
+});
